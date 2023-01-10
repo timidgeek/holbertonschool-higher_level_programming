7dd73108e1cad6172c40944330fc107545cb5ad7
timidgeek <5568@holbertonstudents.com>
2023-01-10 08:45:56 -0800

    pep8 compliant? we are hopeful.

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:    
+if number < 0:
     last = number % -10
 else:
-    last = number % 10    
+    last = number % 10
 print("Last digit of {:d} is {:d} ".format(number, last), end="")
 if last > 5:
     print("and is greater than 5")
