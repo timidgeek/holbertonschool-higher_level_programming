a517b2c629bf4549ab58aa6fe7ae464322a4af8b
timidgeek <5568@holbertonstudents.com>
2023-01-17 11:06:21 -0800

    not allowed to use .replace... used ord instead

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_new_string = my_string.replace("C", "").replace("c", "")
+    my_new_string = my_string.translate({ord(i): None for i in "cC"})
     return my_new_string
