f5d37ee2c97d152b0e20503c5c4ecaa57e159c67
timidgeek <5568@holbertonstudents.com>
2023-01-17 11:31:35 -0800

    adding :d identifier

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        print("\n".join(" ".join(["{}".format(item) for item in row])
+        print("\n".join(" ".join(["{:d}".format(item) for item in row])
                         for row in matrix))
