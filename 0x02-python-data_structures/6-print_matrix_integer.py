eb22c7332ba9166e51f7796c3fb3539e5a80be6a
timidgeek <5568@holbertonstudents.com>
2023-01-17 11:30:11 -0800

    does the matrix exist? edgecase

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print("\n".join(" ".join(["{}".format(item) for item in row])
-                    for row in matrix))
+    if matrix:
+        print("\n".join(" ".join(["{}".format(item) for item in row])
+                        for row in matrix))
