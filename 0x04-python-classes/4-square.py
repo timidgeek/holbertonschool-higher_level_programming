453ea64c0f129bd40d853bc6f34e892a3d578d95
timidgeek <5568@holbertonstudents.com>
2023-01-17 20:26:12 -0800

    final tasks

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a class Square given parameters"""
+
+
+class Square:
+    """Declare the square"""
+    def __init__(self, size=0):
+        """Initialize new square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Return size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the square's size"""
+        if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+        elif size < 0:
+                raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area of the square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the '#' character"""
+        for i in range(self.__size):
+            print(self.__size * "#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
