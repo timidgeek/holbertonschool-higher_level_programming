ed79ea4f32c88e2e54795589dd2014cfb95d779a
timidgeek <5568@holbertonstudents.com>
2023-01-17 19:50:48 -0800

    initial push - readme and task 0

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Represent the Square"""
+    def __init__(self, size):
+        """Define the size"""
+        self.__size = size
