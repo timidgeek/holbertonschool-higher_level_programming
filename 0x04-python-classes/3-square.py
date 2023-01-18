e044370573549a95628c9adb4f9a9094f919e07f
timidgeek <5568@holbertonstudents.com>
2023-01-17 20:29:17 -0800

    missing semicolon ln 17

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -14,7 +14,7 @@ class Square:
         return (self.__size)
 
     @size.setter
-    def size(self, value)
+    def size(self, value):
         """Sets the square's size"""
         if not isinstance(value, int):
                 raise TypeError("size must be an integer")
