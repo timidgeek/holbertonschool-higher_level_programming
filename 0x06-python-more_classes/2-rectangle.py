7245ebfb8d753821ba0aaf5fba5287700518f19d
timidgeek <5568@holbertonstudents.com>
2023-01-24 16:32:19 -0800

    possible commenting error - wrong place

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -3,10 +3,8 @@
 
 
 class Rectangle:
-    """Represent a rectangle"""
-
     def __init__(self, width=0, height=0):
-
+        """Represent a rectangle"""
         self.width = width
         self.height = height
 
