853d527e1d466aba889115851aa78305f53bd1f0
timidgeek <5568@holbertonstudents.com>
2023-01-24 19:22:04 -0800

    initial commit for all functions

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    """Returns the integer addition of a + b"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
