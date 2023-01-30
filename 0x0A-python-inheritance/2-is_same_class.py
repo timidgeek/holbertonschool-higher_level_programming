6e6f155a229f5c34398be1655143e43e92edbc97
timidgeek <5568@holbertonstudents.com>
2023-01-30 12:59:13 -0800

    more simple, removing isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,7 +7,7 @@ specified class ; otherwise False"""
 def is_same_class(obj, a_class):
     """Tests if the object is an
     instance of the specified class"""
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
