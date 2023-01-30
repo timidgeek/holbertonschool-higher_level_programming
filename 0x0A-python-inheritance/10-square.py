2cd76edc0a5ee32bb211960b5e7bc831ecd2d275
timidgeek <5568@holbertonstudents.com>
2023-01-30 14:25:02 -0800

    initial commit task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""New class Square that inherits from
+    Rectangle of ``9-rectangle.py``"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from Rectangle, initializes size"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Calculates area of rectangle"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Will print rectangle dimmensions and squarea"""
+        return "[Rectangle] {}/{}".format(self.__size, self.__size) 
