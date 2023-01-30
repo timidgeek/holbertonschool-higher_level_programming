1277e5f48e1e81db8dd24030adc152b2b1c85317
timidgeek <5568@holbertonstudents.com>
2023-01-30 10:31:48 -0800

    module documentation, pep8 formatting, whitespace removal

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This is the lookup module, which returns
+the list of available attributes and methods
+of an object.
+"""
 
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """Simple function that returns all
+    attributes and methods of list 'obj'"""
     return dir(obj)
