0095b91ce5ab61e180e3cb72d6561745ca6713bc
timidgeek <5568@holbertonstudents.com>
2023-02-01 08:43:51 -0800

    removing space inside end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     and prints it to stdout"""
 
     with open(filename, encoding="utf-8") as file:
-        print(file.read(), end=" ")
+        print(file.read(), end="")
