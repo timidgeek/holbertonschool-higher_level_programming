8a9da3aae6e5741fe5b11d3c1b8671af7a164404
timidgeek <5568@holbertonstudents.com>
2023-02-13 08:14:14 -0800

    removing 'use' to hopefully gain checks

--- 0x00-SQL_introduction/3-list_tables.sql
+++ 0x00-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,3 @@
 -- 3-list_tables.sql, Lindsey Thomas C19
 -- Script that lists all the tables of a database in your MySQL server
-USE mysql;
 SHOW tables;
--- 0x00-SQL_introduction/5-full_table.sql
+++ 0x00-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- 5-full_table.sql, Lindsey Thomas C19
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
-USE hbtn_0c_0;
 SHOW COLUMNS FROM first_table;
