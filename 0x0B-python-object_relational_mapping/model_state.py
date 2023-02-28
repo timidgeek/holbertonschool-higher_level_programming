a1588100a0f34e46e377e04fefda3f04a31af961
timidgeek <5568@holbertonstudents.com>
2023-02-27 22:06:58 -0800

    adding documentation, making string non nullable, removing MySQLdb import.

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -2,12 +2,13 @@
 """State BaseModel"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import MySQLdb
 
 Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
+    """Represents a state from MySQL database"""
+
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128))
+    name = Column(String(128), nullable=False)
