0d1c44a2563f9aabc803b0b7c29aefc65f38b1c1
timidgeek <5568@holbertonstudents.com>
2023-03-01 09:49:12 -0800

    adding BINARY to task 2, joining states and cities tables for task 4

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+    cursor.execute("SELECT * FROM states WHERE name = BINARY '{}' \
                    ORDER BY states.id ASC;"
                    .format(sys.argv[4]))
 
