651cd9ef1be3253bc75b90a76c4454b648b253bb
timidgeek <5568@holbertonstudents.com>
2023-03-01 11:29:20 -0800

    metadata

--- 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,13 +18,14 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, database),
                            pool_pre_ping=True)  # checks if connection invalid
+    Base.metadata.create_all(engine)
 
     # Create session factory bound to the engine
     Session = sessionmaker(bind=engine)
     session = Session()
 
     for state in session.query(State):
-        if "a" in state.name:
+        if "a" in state.name:    
             session.delete(state)
     session.commit()
 
