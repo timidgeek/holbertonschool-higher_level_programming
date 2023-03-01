4dfb9c59ede78239623c4dc0f0c6a44ea6f554d7
timidgeek <5568@holbertonstudents.com>
2023-03-01 09:05:52 -0800

    it was the pycode, and it was the binary.

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE \
+                   BINARY 'N%' ORDER BY id ASC")
     gettem = cursor.fetchall()
 
     for state_name in gettem:
