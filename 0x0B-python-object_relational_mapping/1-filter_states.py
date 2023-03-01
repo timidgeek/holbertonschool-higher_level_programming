26cbb30f87dbac86c861ac3a29b5b3a3c260222e
timidgeek <5568@holbertonstudents.com>
2023-02-28 16:06:47 -0800

    check chasing, fetching in task one, pycodestyle in task 8

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    gettem = cursor.fetchall()
 
-    for state_name in cursor:
+    for state_name in gettem:
         print(state_name)
+
+    db.close()
--- 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     first_state = session.query(State).order_by(State.id).first()
 
     if first_state is None:
-            print("Nothing")
+        print("Nothing")
     else:
         print("{}: {}".format(first_state.id, first_state.name))
