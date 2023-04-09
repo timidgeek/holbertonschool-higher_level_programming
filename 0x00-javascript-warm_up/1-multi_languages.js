44af98ee7957f90f2ec1ad72f3430546fd40b1b5
timidgeek <5568@holbertonstudents.com>
2023-04-08 20:57:12 -0700

    humble beginnings

---
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cProg = 'C is fun\t';
+const pProg = 'Python is cool\t';
+const jProg = 'JavaScript is amazing';
+console.log(cProg);
+console.log(pProg);
+console.log(jProg);
