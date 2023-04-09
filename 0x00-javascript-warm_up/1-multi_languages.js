935f9eda961523a5ecc4ca17294c6571381c712a
timidgeek <5568@holbertonstudents.com>
2023-04-08 20:58:39 -0700

    missing output check because of unnecesary tab

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const cProg = 'C is fun\t';
-const pProg = 'Python is cool\t';
+const cProg = 'C is fun';
+const pProg = 'Python is cool';
 const jProg = 'JavaScript is amazing';
 console.log(cProg);
 console.log(pProg);
