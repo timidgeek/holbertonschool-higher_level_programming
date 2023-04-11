c6c98f4df1b513fc6baf6ffde50c4e8b43d91524
timidgeek <5568@holbertonstudents.com>
2023-04-11 11:26:06 -0700

    initial commit for remaining tasks, semistandard compliant

---
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const x = parseInt(args[0]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
