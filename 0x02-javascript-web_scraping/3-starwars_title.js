f0ebfd98847ca9f0325e9d52835ec0e111ad2131
timidgeek <5568@holbertonstudents.com>
2023-04-17 11:30:35 -0700

    never forget the power of the  back ticks

---
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (err, response, body) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    const data = JSON.parse(body); /* parse and store data from given star wars api */
+    console.log(`Title: ${data.title}`)
+});
