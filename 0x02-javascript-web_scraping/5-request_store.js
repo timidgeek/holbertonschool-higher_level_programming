7055cc13d8aebf8cf516d82b83353f2d31f573d1
timidgeek <5568@holbertonstudents.com>
2023-04-18 20:10:54 -0700

    remove console log, reformat

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -4,17 +4,18 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
-  fs.writeFile(filePath, body, 'utf-8', function (err) {
+if (process.argv.length < 4) {
+  console.error('not enough arguments');
+} else {
+  request.get(url, (err, response, body) => {
     if (err) {
       console.error(err);
-      process.exit(1);
+      return;
     }
-    console.log(`${filePath}`);
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
   });
-});
+}
